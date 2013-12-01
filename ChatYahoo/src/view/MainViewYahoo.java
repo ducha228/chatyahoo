c3efc28694b92a9d90a3cb43d0432cfc69c2ba3b
NguyenDucHa <ducha@yopmail.com>
2013-12-01 10:59:28 +0700

    fix conflict mainview

--- ChatYahoo/src/view/MainViewYahoo.java
+++ ChatYahoo/src/view/MainViewYahoo.java
@@ -8,12 +8,16 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -23,6 +27,7 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
+import controlClient.ImageManager;
 import model.User;
 
 public class MainViewYahoo extends JFrame {
@@ -35,11 +40,10 @@ public class MainViewYahoo extends JFrame {
 	private DefaultListModel dlm;
 	private User user;
 	private Image avatar;
-	public MainViewYahoo(User user,Image avatar) {
+	public MainViewYahoo(User user) {
 		// TODO Auto-generated constructor stub
 		super("ChatOnline");
 		this.user = user;
-		this.avatar = avatar;
 		MyPanel pnlChat = new MyPanel("backgroundview.jpg");
 		pnlChat.setPreferredSize(new Dimension(250, 500));
 
@@ -127,4 +131,38 @@ public class MainViewYahoo extends JFrame {
 			dlm.addElement(string);
 		}
 	}
+	public void addChooseAvatar(MouseListener ml) {
+		lblAvatar.addMouseListener(ml);
+	}
+	
+	public File showAvatarChooser() {
+		JFileChooser filechooser = new JFileChooser();
+//		filechooser.addChoosableFileFilter(new FileNameExtensionFilter("Images",
+//				"jpg", "png", "gif", "bmp"));
+		int returnVal = filechooser.showOpenDialog(MainViewYahoo.this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = filechooser.getSelectedFile();
+//			String mimeType = new MimetypesFileTypeMap().getContentType(file);
+//			   // mimeType should now be something like "image/png"
+//
+//			   if(mimeType.substring(0,5).equalsIgnoreCase("image")){
+//			         // its an image
+//				   return file;
+//			   } else {
+//				   showAvatarChooser();
+//			   }
+			return file;
+		}
+		return null;
+	}
+	
+	public void setAvatar(BufferedImage avatar) {
+		avatar = ImageManager.getScaledImage(avatar, lblAvatar.getWidth(), lblAvatar.getHeight());
+		lblAvatar.setIcon(new ImageIcon(avatar));
+	}
+
+	public void setDefaultAvatar() {
+		lblAvatar.setIcon(new ImageIcon("avatar.jpg"));
+	}
+
 }
